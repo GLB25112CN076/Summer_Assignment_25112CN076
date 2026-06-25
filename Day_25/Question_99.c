8763db1f41e3066d46d7e70bb6e5faa226f09f01
GLB25112CN076 <gautamverma1106@gmail.com>
2026-06-25 21:17:07 +0530

    Day 25 Task Complete

---
+++ Day_25/Question_99.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char names[5][50], temp[50];
+    int i, j;
+
+    printf("Enter 5 names:\n");
+    for(i = 0; i < 5; i++) {
+        gets(names[i]);
+    }
+
+    // Sorting names alphabetically
+    for(i = 0; i < 5 - 1; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    printf("\nNames in alphabetical order:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%s\n", names[i]);
+    }
+
+    return 0;
+}
